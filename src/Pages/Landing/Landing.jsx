bbc9d69fbeef8dfc1e4bcea3751d2f336dcdc790
princechibez <chibezprince@gmail.com>
2023-05-05 05:44:53 -0700

    improved page loader

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import './Landing.scss'
 import Hero from '../../Components/Hero/Hero'
 import Navbar from '../../Components/Navbar/Navbar'
-import Loading from '../Loading/Loading'
 
 const Landing = () => {
   return (
     <div className='landing'>
         {/* <Navbar /> */}
         <Hero />
-        {/* <Loading /> */}
     </div>
   )
 }
--- src/Pages/Loading/Loading.jsx
+++ src/Pages/Loading/Loading.jsx
@@ -1,6 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import hat from '../../assets/hat.png';
-import './Loading.scss';
+import React, { useEffect, useState } from "react";
+import hat from "../../assets/hat.png";
+import "./Loading.scss";
+import { Box } from "@mui/system";
+import { CircularProgress, Typography } from "@mui/material";
+
+const CircularProgressWithLabel = (props) => {
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        display: "inline-flex",
+      }}
+    >
+      <CircularProgress size={70} variant="determinate" {...props} />
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: "absolute",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Typography
+          variant="caption"
+          fontSize={14}
+          fontWeight={600}
+          component="div"
+          sx={{ color: "#FFFFFF" }}
+        >
+          {`${Math.round(props.value)}%`}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
 
 const Loading = () => {
   const [percentage, setPercentage] = useState(0);
@@ -14,7 +51,7 @@ const Loading = () => {
       const deltaTime = currentTime - lastRenderTime;
       lastRenderTime = currentTime;
 
-      setPercentage(prevPercentage => {
+      setPercentage((prevPercentage) => {
         const newPercentage = prevPercentage + deltaTime / 10;
         if (newPercentage >= 100) {
           return 100;
@@ -26,10 +63,10 @@ const Loading = () => {
       rafId = requestAnimationFrame(render);
     };
 
-    const img = document.querySelector('.loading img');
-    img.classList.add('animated');
+    const img = document.querySelector(".loading img");
+    img.classList.add("animated");
 
-    const app = document.querySelector('.App');
+    const app = document.querySelector(".App");
     const observer = new MutationObserver(() => {
       if (app.childElementCount === 0) {
         clearInterval(interval);
@@ -40,10 +77,10 @@ const Loading = () => {
     observer.observe(app, { childList: true });
 
     const interval = setInterval(() => {
-      const img = document.querySelector('.loading img');
-      img.classList.remove('animated');
+      const img = document.querySelector(".loading img");
+      img.classList.remove("animated");
       void img.offsetWidth;
-      img.classList.add('animated');
+      img.classList.add("animated");
     }, 2000);
 
     render();
@@ -56,9 +93,12 @@ const Loading = () => {
   }, []);
 
   return (
-    <div className='loading'>
-      <img src={hat} alt='' />
-      <span>{Math.min(Math.round(percentage), 100)}%</span>
+    <div className="loading">
+      <img src={hat} alt="Loader Image" />
+      <CircularProgressWithLabel
+        value={Math.min(Math.round(percentage), 100)}
+      />
+      {/* <span>{Math.min(Math.round(percentage), 100)}%</span> */}
     </div>
   );
 };
