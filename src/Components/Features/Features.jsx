cde4dbca0e00426dd38bee69dfec2481a6c3a184
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2023-05-05 16:54:45 +0100

    features section

---
+++ src/Components/Features/Features.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import './Features.scss';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Features = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://lacocina-api.onrender.com/api/v1/recipe/getAllRecipes')
+      .then(response => {
+        // console.log(response.data);
+        setRecipes(response.data);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        // console.error(error);
+        setError(error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading recipes...</div>;
+  }
+
+  if (error) {
+    return <div className="error-message">Failed to load recipes: {error.message}</div>;
+  }
+
+  return (
+    <div className='features'>
+      <h1>Featured Recipe</h1>
+      <p>Try making meals from our featured recipes</p>
+
+        <div className='container'>
+            {Array.isArray(recipes) && recipes.map(recipe => {
+                console.log(recipe); // added console.log statement
+                return (
+                    <div key={recipe.id}>
+                    <h2>{recipe.title}</h2>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <p>{recipe.dishType}</p>
+                    </div>
+                )
+            })}
+        </div>
+
+
+      <Link to='/recipe' className='link'>Get cooking</Link>
+    </div>
+  );
+};
+
+export default Features;
--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -4,12 +4,14 @@ import Hero from '../../Components/Hero/Hero'
 import Navbar from '../../Components/Navbar/Navbar'
 import Loading from '../Loading/Loading'
 import Footer from '../../Components/Footer/Footer'
+import Features from '../../Components/Features/Features'
 
 const Landing = () => {
   return (
     <div className='landing'>
         {/* <Navbar /> */}
         <Hero />
+        <Features />
         {/* <Loading /> */}
         <Footer />
     </div>
